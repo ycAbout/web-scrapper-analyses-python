04fbafc1f7325c323b97307783d55788f179f9d5
yc <yc.about@gmail.com>
2018-09-23 15:13:26 -0600

    add history & plot function

--- README.md
+++ README.md
@@ -1,9 +1,16 @@
 This is a simple demo for web scrapper using python and BeautifulSoup4.
 
-This app simply gets the stock price of some tech companies from NASDAQ.
+# Usage
+This app gets
+1. the `curent stock price` of the selected stock, and
+2. the `historical stock price` and display it in a figure 
+of some tech companies from NASDAQ.
+
+You can 
 
 Urllib is a standard python3 library to acquire HTML.
 BeautifulSoup4 get the elements from the HTML acquired.
-Retrieve 1 record 1 second to reduce the server load.
+matplotlib plot a figure.
+Please retrieve 1 record 1 second to reduce the server load.
 
 Happy using it.
--- scrapper.py
+++ scrapper.py
@@ -3,6 +3,9 @@ import urllib
 import time
 #For scrapping
 from bs4 import BeautifulSoup
+#For plot
+import matplotlib.pyplot as plt
+
 
 symbols = [
   'https://www.nasdaq.com/symbol/fb',
@@ -14,13 +17,58 @@ symbols = [
   'https://www.nasdaq.com/symbol/amzn',
 ]
 
-for symbol in symbols:
-  page = urllib.request.urlopen(symbol)
-  soup = BeautifulSoup(page,'html.parser')
+def getHistory(link):
+  """take in a link corresponding to history price
+  return a list with history price [[date, open, high, low, close, volume],...]
+  """
+  historyPage = urllib.request.urlopen(link)
+  soup = BeautifulSoup(historyPage,'html.parser')
+  #get the history price table for last three months
+  table = soup.find(id="historicalContainer").table
+  body = table.tbody
+  row = []
+  historyPrice = []
+  for string in body.stripped_strings:
+    row.append(string)
+    # every row tr contains 6 td elements
+    if len(row) == 6:
+      #using copy to generate a new list, otherwise list will be cleared.
+      historyPrice.append(row.copy())  
+      row.clear()
+  return historyPrice
 
-  name = soup.find(id="qwidget_pageheader").get_text().split(' ')[0] + '               '
-    
-  #last sale price
-  price = soup.find(id="qwidget_lastsale").get_text()
-  print(name[:15], price)
-  time.sleep(1)
+def plot(name, x,y):
+  """take in list x and list y to make a plot
+  print a plot
+  """
+  plt.plot(x,y)
+  plt.title(name)
+  plt.ylabel('price')
+  plt.xlabel('days before')
+  plt.show()
+
+def main():
+  for symbol in symbols:
+    page = urllib.request.urlopen(symbol)
+    soup = BeautifulSoup(page,'html.parser')
+  
+    name = soup.find(id="qwidget_pageheader").get_text().split(' ')[0]
+      
+    #last sale price
+    price = soup.find(id="qwidget_lastsale").get_text()
+    print(name, price)
+  #  time.sleep(1)
+    historyLink = soup.find(id="historical_quoteslink").get('href')
+    historyPrice = getHistory(historyLink)
+    # x into trading days before the current trading day
+    x = []
+    for i in range(len(historyPrice)):
+      x.append(0-i)
+    # y for price    
+    y = []
+    for item in historyPrice:
+      y.append(float(item[4]))
+
+    plot(name, x,y)
+
+main()
